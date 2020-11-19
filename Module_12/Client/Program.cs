8fcf25a51bc7a2f667a1e78220aef5549a779551
Patrick Schmidt <pschmidt@pasit.nl>
2020-11-19 16:08:35 +0100

    Versionning

---
+++ Module_12/Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StringUtils;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "Hello World";
+            string reverse = x.Keerom();
+            Console.WriteLine(reverse);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Module_12/StringUtils/Utilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringUtils
+{
+    public static class Utilities
+    {
+        public static string Keerom(this string s)
+        {
+            char[] tmp = s.ToCharArray();
+            Array.Reverse(tmp);
+            return new string(tmp).ToUpper();
+        }
+    }
+}
